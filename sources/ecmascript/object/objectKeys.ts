6b7ed8a7f8877ee6c3fbfaa9214dcd44c20c2592
Alexandr <digital-mag@ya.ru>
2023-06-05 14:51:38 +0300

    apply unsafe keys method

--- sources/ecmascript/object/objectKeys.ts
+++ sources/ecmascript/object/objectKeys.ts
@@ -1,23 +1,30 @@
+/// look up readme section on Object.keys 
+
+
+// type unsafe = 'unsafe';
+
 interface ObjectConstructor {
     /**
      * Returns the names of the enumerable string properties and methods of an object.
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
     // keys<T extends object>(o: T): Array<keyof T> & string[];
+
+    getOwnPropertyNames<T extends object>(o: T): T extends Required<T>
+        ? string extends T ? string[] : ReadonlyArray<keyof T>
+        : string[];
+
+    keys<T extends object>(o: T): T extends Required<T>
+        ? string extends T ? string[] : ReadonlyArray<keyof T>
+        : string[];
     
-    getOwnPropertyNames<T extends object>(o: T): T extends Required<T> ? Array<keyof T> : string[];
-    
-    keys<T extends object>(o: T): T extends Required<T> ? Array<keyof T> : string[];
-    entries<T extends object>(o: T): Array<[keyof T, T[keyof T]]>;
+    entries<T extends object>(o: T): T extends Required<T>
+        ? string extends T ? string[] : Array<[keyof T, T[keyof T]]>
+        : string[];
+
+    // keys<T extends 'unsafe'|'safe', O extends object>(o: O): T extends 'unsafe' ? Array<keyof O> : string[];
+
 }
 
 
-type A = {
-    a?: string,
-    b: number
-}
-let aa: A = {
-    a: '',
-    b: 1
-}
-const rr = Object.keys(aa)
+
